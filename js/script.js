101b3ba314c590ce2beb36dbe9d212792a8e9392
HollowDS <denis.ospov.2017@inbox.ru>
2023-11-11 21:01:39 +0300

    add second page

--- js/script.js
+++ js/script.js
@@ -21,56 +21,12 @@ $("a[href^='#']").on("click", function () {
 	return false;
 });
 
-// Popup variables
-const popupCloseIcon = document.querySelector(".close-popup");
-const popupLink = document.querySelector(".popup-link");
-const popup = document.querySelector(".popup");
-const body = document.querySelector("body");
-
-// Popup code
-popupLink.addEventListener("click", function (e) {
-	popupOpen(popup);
-	e.preventDefault();
-});
-
-popupCloseIcon.addEventListener("click", function (e) {
-	popupClose(popup);
-	e.preventDefault();
-});
-
-function popupOpen(curentPopup) {
-	curentPopup.classList.add("open");
-	popupLink.style["display"] = "none"
-	popupCloseIcon.style["display"] = "block"
+const burger = document.querySelector('.header__burger');
+if (burger){
+	const navigation = document.querySelector('.navigation__list');
+	burger.addEventListener('click', function(e) {
+		document.body.classList.toggle('_lock');
+		burger.classList.toggle('_active');
+		navigation.classList.toggle('_active');
+	})
 }
-
-function popupClose(popupActive) {
-	popupActive.classList.remove("open");
-	popupLink.style["display"] = "block"
-	popupCloseIcon.style["display"] = "none"
-}
-// End popup code
-
-// Mouse move
-ns4 = (document.layers)? true:false
-ie4 = (document.all)? true:false
-
-function init() {
-    if (ns4) {document.captureEvents(Event.MOUSEMOVE);}
-    document.onmousemove=mousemove;
-}
-function mousemove(event) {
-    var mouse_x = y = 0;
-    if (document.attachEvent != null) {
-        mouse_x = window.event.clientX;
-        mouse_y = window.event.clientY;
-    } else if (!document.attachEvent && document.addEventListener) {
-        mouse_x = event.clientX-0;
-        mouse_y = event.clientY-0;
-    }
-    document.getElementById('greeting').style.backgroundPosition= 'calc('+mouse_x/20+'px - 100vw) calc('+mouse_y/20+'px - 100vw)';
-  
-}
-  init()
-
-// 
